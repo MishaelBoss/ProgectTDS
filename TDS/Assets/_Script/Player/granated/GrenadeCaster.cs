febdac31988835b1a0644e1aa5bb20a7cde9519a
Michael <michaelvoov@yandex.ru>
2024-03-23 13:34:09 +0500

    Update

---
+++ TDS/Assets/_Script/Player/granated/GrenadeCaster.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeCaster : MonoBehaviour
+{
+    public Rigidbody prefabs;
+    public Transform Transform;
+
+    public float Force = 200;
+    void Update()
+    {
+        CheckInputButtonCretaeGranate();
+    }
+
+    void CheckInputButtonCretaeGranate() {
+        if (Input.GetMouseButtonDown(1)) { 
+            var granade = Instantiate(prefabs);
+            granade.transform.position = Transform.position;
+            granade.GetComponent<Rigidbody>().AddForce(Transform.forward * Force);
+        }
+    }
+}
